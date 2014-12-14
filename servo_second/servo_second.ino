10edf30f9d4ab14895a8852586fedc66ea88358a
unknown <rdedhia@D6WPFV1.olin.edu>
2014-12-14 01:51:59 -0500

    changed servo_second script such that it changes depending on color argument communicated over pyserial

--- servo_second/servo_second.ino
+++ servo_second/servo_second.ino
@@ -2,7 +2,10 @@
 
 Servo loadServo;
 int pos;
+int pos_min;
+int pos_max;
 String incoming = "";
+int color; // 0 is blue, 1 is orange, 2 is yellow
 
 void setup() {
   Serial.begin(9600);
@@ -13,23 +16,32 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) { //check for new data
     incoming = Serial.readString();
-    load();
+    color = incoming.toInt();
+    load(color);
   }
 }
 
-
-void load() 
+void load(int color)
 { 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  if (color == 0) {
+    pos_min = 0;
+    pos_max = 90;  
+  } else if (color == 1) {
+    pos_min = 90;
+    pos_max = 180;
+  } else {
+    pos_min = 180;
+    pos_max = 270;
+  }  
+  for(pos = pos_min; pos <= pos_max; pos += 1)  // goes from 0 degrees to 180 degrees 
   {                                  // in steps of 1 degree 
     loadServo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(10);                       // waits 15ms for the servo to reach the position 
   }
-  for(pos = 180; pos>= 0; pos -= 1)     // goes from 180 degrees to 0 degrees 
+  for(pos = pos_max; pos >= pos_min; pos -= 1)     // goes from 180 degrees to 0 degrees 
   {                                
     loadServo.write(pos);               // tell servo to go to position in variable 'pos' 
     delay(10);                       // waits 15ms for the servo to reach the position 
   }
-  delay(200);
 }
 
