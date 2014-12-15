a9237cfd0af2f459af5867c059e2b8fc9870dd95
Riley Chapman <rlychpmn@gmail.com>
2014-12-14 20:08:30 -0500

    reworked sherical conversion

--- dual_motor_control/test.py
+++ dual_motor_control/test.py
@@ -3,20 +3,34 @@ import math
 def arctan(x):
     return math.degrees(math.atan(x))
 
-def to_spherical(distance, xcenter, height, omega, phi, x, y):
+def to_spherical(x, y):
     # dimensions are ~100 x 100
     # go from 20 to 90
     # omega is zeroing angle from the horizontal (on pan)
-    # phi is the same, but on tilt    
+    # phi is the same, but on tilt   
+    
+    # Everything is in inches
+    distance = 56.
+    height = 14
+    omega = 31
+    phi = 50
+    tWidth=38
+    tHeight=40
+    xmin=omega-arctan(0.5*tWidth/distance)
+    xmax=omega+arctan(0.5*tWidth/distance)
+    ymin=phi-arctan((height-6)/distance)
+    ymax=phi+arctan((tWidth-height)/distance)
 
-    x_angle = arctan((x - xcenter) / distance) + omega
-    y_angle = arctan((y - height) / distance) + phi
+    x_angle = arctan((x - tWidth/2) / distance) + omega
+    y_angle = -arctan((y - tHeight+height) / distance) + phi
+    if x_angle<xmin:
+        x_angle=xmin
+    if x_angle>xmax:
+        x_angle=xmax
+    if y_angle<ymin:
+        y_angle=ymin
+    if y_angle>ymax:
+        y_angle=ymax    
     return x_angle, y_angle
 
-# Everything is in inches
-distance = 56.
-xcenter = 20
-height = 14
-omega = 30
-phi = 50
-print to_spherical(distance, xcenter, height, omega, phi, 40, 40)
+print to_spherical(40,0)
